efe2247258b17cde1ceb365aa9d0fd79b1593dda
Dean <ffleagueplease@gmail.com>
2026-03-08 14:50:39 +0000

    the window opens with text, yay !

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,24 @@
 #include "raylib.h"
 
 int main() {
+    
     InitWindow(800, 450, "My First C++ Game");
     SetTargetFPS(60);
+    
+    const char* displayText = " CHANGE";
+    int fontSize = 20;
+    int posX = GetScreenWidth()/2 - MeasureText(displayText, fontSize)/2;
+    int posY = GetScreenHeight()/2 - (fontSize / 2);
+    Color color = BLACK;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! Your dev environment is live.", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(Color{ 255, 20, 30, 255 });
+            DrawText(displayText,
+            posX, 
+            posY,
+            fontSize, 
+            color);
         EndDrawing();
     }
 
