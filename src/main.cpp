176020af5d96d7db0cb6b615629b64a3100128ff
Dean <ffleagueplease@gmail.com>
2026-03-08 15:32:21 +0000

    Rectangle with text.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,43 @@
-// Ctrl + Shift + B
-
 #include "raylib.h"
-
+#include <iostream>
+#define MOUSE_SCALE_MARK_SIZE   12
 int main() {
-    
     InitWindow(800, 450, "My First C++ Game");
     SetTargetFPS(60);
+
+    Rectangle rec = { 100, 100, 200, 80 };
+    Vector2 mousePosition = { 0 };
+
+    bool mouseScaleReady = false;
+    bool mouseScaleMode = false;
     
     const char* displayText = " CHANGE";
     int fontSize = 20;
     int posX = GetScreenWidth()/2 - MeasureText(displayText, fontSize)/2;
     int posY = GetScreenHeight()/2 - (fontSize / 2);
-    Color color = BLACK;
+    Color background = Color{ 38, 42, 60, 255};
+    Color recColour = Color{ 84, 68, 43, 255};
 
     while (!WindowShouldClose()) {
+        mousePosition = GetMousePosition();
+        if (CheckCollisionPointRec(mousePosition, rec))
+        {
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) 
+            {
+                mouseScaleMode = true;
+                std::cout << "Mouse Clicked - @" << mousePosition.x << ", " << mousePosition.y << std::endl;
+            }    
+        }
+        else mouseScaleReady = false;
+
         BeginDrawing();
-            ClearBackground(Color{ 255, 20, 30, 255 });
+            ClearBackground(background);
             DrawText(displayText,
             posX, 
             posY,
             fontSize, 
-            color);
+            recColour);
+            DrawRectangleRec(rec, Fade(recColour, 0.5f));
         EndDrawing();
     }
 
