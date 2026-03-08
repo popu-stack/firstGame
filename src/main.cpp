b830978b197f74ac8aaa18e77c3a9476627d8cb1
Dean <ffleagueplease@gmail.com>
2026-03-08 14:22:51 +0000

    Folder clean up

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// Ctrl + Shift + B
+
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 450, "My First C++ Game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! Your dev environment is live.", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
