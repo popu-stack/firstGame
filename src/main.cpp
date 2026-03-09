63f60196359da56db12b9ee713bbbe9bf9f0ca64
Dean <ffleagueplease@gmail.com>
2026-03-09 09:12:25 +0000

    Workspace include added

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,50 @@
 #include "raylib.h"
 #include <iostream>
-#define MOUSE_SCALE_MARK_SIZE   12
+
+void CaptureScreen(const char* filename) {
+    const char* appDir = GetApplicationDirectory(); 
+    const char* screenshotFolder = TextFormat("%s../screenshots", appDir);
+    if (!DirectoryExists(screenshotFolder)) {
+        MakeDirectory(screenshotFolder);
+    }
+    const char* fullPath = TextFormat("%s/%s", screenshotFolder, filename);
+    Image screenshot = LoadImageFromScreen();
+    ExportImage(screenshot, fullPath);
+    UnloadImage(screenshot);
+    TraceLog(LOG_INFO, "Screenshot successfully saved to: %s", fullPath);
+}
+
 int main() {
     InitWindow(800, 450, "My First C++ Game");
     SetTargetFPS(60);
-
     Rectangle rec = { 100, 100, 200, 80 };
     Vector2 mousePosition = { 0 };
-
-    bool mouseScaleReady = false;
     bool mouseScaleMode = false;
-    
-    const char* displayText = " CHANGE";
+    const char* appDir = GetApplicationDirectory();
+    const char* displayText = "Lord Morgan, 2024...";
     int fontSize = 20;
     int posX = GetScreenWidth()/2 - MeasureText(displayText, fontSize)/2;
     int posY = GetScreenHeight()/2 - (fontSize / 2);
-    Color background = Color{ 38, 42, 60, 255};
+    
+    Color background = Color{ 69, 54, 75, 255};
     Color recColour = Color{ 84, 68, 43, 255};
+    Color textColour = { 181, 194, 183, 255 };
 
     while (!WindowShouldClose()) {
         mousePosition = GetMousePosition();
-        if (CheckCollisionPointRec(mousePosition, rec))
-        {
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) 
-            {
+
+        if (CheckCollisionPointRec(mousePosition, rec)) {
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                 mouseScaleMode = true;
                 std::cout << "Mouse Clicked - @" << mousePosition.x << ", " << mousePosition.y << std::endl;
-            }    
+                CaptureScreen("screenshot.png");
+            };    
         }
-        else mouseScaleReady = false;
-
         BeginDrawing();
             ClearBackground(background);
-            DrawText(displayText,
-            posX, 
-            posY,
-            fontSize, 
-            recColour);
+            DrawText(displayText, posX, posY, fontSize, textColour);
             DrawRectangleRec(rec, Fade(recColour, 0.5f));
+            DrawText("SAVE SCREEN", rec.x + 10, rec.y + 30, 20, WHITE);
         EndDrawing();
     }
 
