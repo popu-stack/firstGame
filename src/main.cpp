123db37a1f4e8bbb9a4fa63ac0504ae8343bba82
Dean <ffleagueplease@gmail.com>
2026-03-17 16:31:30 +0000

    Pressing left click takes screenshot

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,62 @@
 #include "raylib.h"
+#include <filesystem>
 #include <iostream>
 
-void CaptureScreen(const char* filename) {
+
+int removeFile(){
+    // escape backslashes or use a raw string literal so the path is valid
+    std::filesystem::remove(R"(C:\Users\hayde\Documents\Projects\firstgame\myfile.txt)");
+    return 0;
+}
+
+void CaptureScreen(const char* baseName) {
     const char* appDir = GetApplicationDirectory(); 
-    const char* screenshotFolder = TextFormat("%s../screenshots", appDir);
-    if (!DirectoryExists(screenshotFolder)) {
-        MakeDirectory(screenshotFolder);
+    const char* folderPath = TextFormat("%s../screenshots", appDir);
+
+    if (!DirectoryExists(folderPath)) {
+        MakeDirectory(folderPath);
+        std::cout << "Folder created at: " << folderPath << std::endl;
     }
-    const char* fullPath = TextFormat("%s/%s", screenshotFolder, filename);
+
+    const char* fileName = TextFormat("cap_%0.0f_%s", GetTime(), baseName);
+    const char* fullPath = TextFormat("%s/%s", folderPath, fileName);
+
+    removeFile();
     Image screenshot = LoadImageFromScreen();
     ExportImage(screenshot, fullPath);
     UnloadImage(screenshot);
-    TraceLog(LOG_INFO, "Screenshot successfully saved to: %s", fullPath);
+    TraceLog(LOG_INFO, "SCREENSHOT SAVED: %s", fullPath);
 }
 
 int main() {
     InitWindow(800, 450, "My First C++ Game");
     SetTargetFPS(60);
+
     Rectangle rec = { 100, 100, 200, 80 };
-    Vector2 mousePosition = { 0 };
-    bool mouseScaleMode = false;
-    const char* appDir = GetApplicationDirectory();
     const char* displayText = "Lord Morgan, 2024...";
-    int fontSize = 20;
-    int posX = GetScreenWidth()/2 - MeasureText(displayText, fontSize)/2;
-    int posY = GetScreenHeight()/2 - (fontSize / 2);
     
-    Color background = Color{ 69, 54, 75, 255};
-    Color recColour = Color{ 84, 68, 43, 255};
+    // Style colors
+    Color background = { 69, 54, 75, 255 };
+    Color recColour = { 84, 68, 43, 255 };
     Color textColour = { 181, 194, 183, 255 };
 
     while (!WindowShouldClose()) {
-        mousePosition = GetMousePosition();
+        Vector2 mousePosition = GetMousePosition();
 
         if (CheckCollisionPointRec(mousePosition, rec)) {
             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                mouseScaleMode = true;
-                std::cout << "Mouse Clicked - @" << mousePosition.x << ", " << mousePosition.y << std::endl;
-                CaptureScreen("screenshot.png");
-            };    
+                // Now saves to ../screenshots/cap_[time]_myshot.png
+                CaptureScreen("myshot.png");
+            }    
         }
+
         BeginDrawing();
             ClearBackground(background);
-            DrawText(displayText, posX, posY, fontSize, textColour);
-            DrawRectangleRec(rec, Fade(recColour, 0.5f));
-            DrawText("SAVE SCREEN", rec.x + 10, rec.y + 30, 20, WHITE);
+            DrawText(displayText, 40, 200, 20, textColour);
+            
+            // Button Drawing
+            DrawRectangleRec(rec, Fade(recColour, 0.8f));
+            DrawText("SAVE SCREEN", rec.x + 35, rec.y + 30, 20, WHITE);
         EndDrawing();
     }
 
